adf87372e0111d3d8b4bf1c637ede3ec2147887c
335170767 <335170767@C-DF02Z72.tdsb.on.ca>
2023-01-18 14:52:04 -0500

    Flags work, clicking works

--- Minesweeper/src/NewButton.java
+++ Minesweeper/src/NewButton.java
@@ -7,12 +7,16 @@ public class NewButton extends Button {
 	private int row;
 	private int col;
 	private boolean isBomb;
-
+	private boolean isFlagged;
+	private boolean isRevealed;  
+	
 	public NewButton(int r, int c) {
 		super();
 		row = r;
 		col = c;
 		isBomb = false;
+		isFlagged = false;
+		isRevealed = false;
 	}
 
 	public int getRow() {
@@ -27,9 +31,26 @@ public class NewButton extends Button {
 	public boolean isBomb() {
 		return isBomb;
 	}
-
+	public boolean isFlagged() {
+		return isFlagged;
+	}
+	public boolean isRevealed() {
+		return isRevealed;
+	}
+	
 	public void makeBomb() {
 		isBomb = true;
+	}
+	
+	public void removeFlag() {
+		isFlagged = false;
+	}
+	
+	public void makeFlagged() {
+		isFlagged = true;
+	}
+	public void reveal() {
+		isRevealed = true;
 	}
 	@Override
 	public String toString(){
--- Minesweeper/src/minesweeper.java
+++ Minesweeper/src/minesweeper.java
@@ -1,12 +1,12 @@
 import java.util.ArrayList;
 import java.util.Random;
-
-import hutchison.grant.NewButton;
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Labeled;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseButton;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import java.lang.Math;
@@ -20,11 +20,22 @@ public class minesweeper extends Application {
 	private final double HEIGHT = 800;
 	private final int ROWS = 8;
 	private final int COLS = 8;
-	private int bombsToBePlaced = 10;
+	private int bombsToBePlaced = 16;
+	private boolean isGameOn = true;
 
 	private NewButton board[][] = new NewButton[ROWS][COLS];
 	Image baseTile = new Image("BaseTile.png");
 	Image bomb = new Image("Bomb.PNG");
+	Image one = new Image("1.png");
+	Image two = new Image("2.png");
+	Image three = new Image("3.png");
+	Image four = new Image("4.png");
+	Image five = new Image("5.png");
+	Image six = new Image("6.png");
+	Image seven = new Image("7.png");
+	Image eight = new Image("8.png");
+	Image checkedTile = new Image("bigGreyTile.png");
+	Image flag = new Image("Flag.png");
 
 	@Override
 	public void start(Stage stage) throws Exception {
@@ -48,13 +59,16 @@ public class minesweeper extends Application {
 
 				// List the coordinates of neighbours when clicked TEST
 				board[i][j].setOnAction(e -> {
-					System.out.println(countNeighbours(getNeighbours((NewButton)e.getSource())));
+
+					System.out.println(countNeighbours(getNeighbours((NewButton) e.getSource())));
+					System.out.println(((NewButton) e.getSource()).isBomb());
 				});
 			}
 		}
 		for (int i = 0; i < ROWS; i++) {
 			for (int j = 0; j < COLS; j++) {
-				gridPane.add(board[i][j], j, i + 1);
+				gridPane.add(board[i][j], j, i);
+
 			}
 		}
 
@@ -78,9 +92,95 @@ public class minesweeper extends Application {
 			for (int j = 0; j < COLS; j++) {
 
 				ImageView bombView = new ImageView(bomb);
-				if (board[i][j].isBomb()) {
-					board[i][j].setGraphic(bombView);
-				}
+				ImageView oneView = new ImageView(one);
+				ImageView twoView = new ImageView(two);
+				ImageView threeView = new ImageView(three);
+				ImageView fourView = new ImageView(four);
+				ImageView fiveView = new ImageView(five);
+				ImageView sixView = new ImageView(six);
+				ImageView sevenView = new ImageView(seven);
+				ImageView eightView = new ImageView(eight);
+				ImageView baseTileView = new ImageView(baseTile);
+				ImageView checkedTileView = new ImageView(checkedTile);
+				ImageView flagView = new ImageView(flag);
+				int count = countNeighbours(getNeighbours(board[j][i]));
+
+				System.out.println(i + " , " + j + " count = " + count);
+
+				board[i][j].setGraphic(baseTileView);
+
+				board[i][j].setOnMouseClicked(e -> {
+					if (e.getButton() == MouseButton.SECONDARY) {
+						if (((NewButton) e.getSource()).isFlagged()) {
+							((NewButton) e.getSource()).removeFlag();
+							((NewButton) e.getSource()).setGraphic(baseTileView);
+						} else if(((NewButton) e.getSource()).isRevealed()== false){
+
+							((NewButton) e.getSource()).setGraphic(flagView);
+							((NewButton) e.getSource()).makeFlagged();
+						}
+					}
+					if (e.getButton() == MouseButton.PRIMARY) {
+						System.out.println(((NewButton) e.getSource()).getCol() + "," + ((NewButton) e.getSource()).getRow());
+						System.out.println(countNeighbours(getNeighbours((NewButton) e.getSource())));
+						System.out.println(((NewButton) e.getSource()).isFlagged());
+
+						if (((NewButton) e.getSource()).isBomb() && isGameOn == true
+								&& ((NewButton) e.getSource()).isFlagged() == false) {
+							((NewButton) e.getSource()).setGraphic(bombView);
+							((NewButton) e.getSource()).reveal();
+							 isGameOn = false;
+
+						}
+						if (count == 1 && !((NewButton) e.getSource()).isBomb() && isGameOn == true
+								&& ((NewButton) e.getSource()).isFlagged() == false) {
+							((NewButton) e.getSource()).setGraphic(oneView);
+							((NewButton) e.getSource()).reveal();
+							
+						}
+						if (count == 2 && !((NewButton) e.getSource()).isBomb() && isGameOn == true
+								&& ((NewButton) e.getSource()).isFlagged() == false) {
+							((NewButton) e.getSource()).setGraphic(twoView);
+							((NewButton) e.getSource()).reveal();
+						}
+						if (count == 3 && !((NewButton) e.getSource()).isBomb() && isGameOn == true
+								&& ((NewButton) e.getSource()).isFlagged() == false) {
+							((NewButton) e.getSource()).setGraphic(threeView);
+							((NewButton) e.getSource()).reveal();
+						}
+						if (count == 4 && !((NewButton) e.getSource()).isBomb() && isGameOn == true
+								&& ((NewButton) e.getSource()).isFlagged() == false) {
+							((NewButton) e.getSource()).setGraphic(fourView);
+							((NewButton) e.getSource()).reveal();
+						}
+						if (count == 5 && !((NewButton) e.getSource()).isBomb() && isGameOn == true
+								&& ((NewButton) e.getSource()).isFlagged() == false) {
+							((NewButton) e.getSource()).setGraphic(fiveView);
+							((NewButton) e.getSource()).reveal();
+						}
+						if (count == 6 && !((NewButton) e.getSource()).isBomb() && isGameOn == true
+								&& ((NewButton) e.getSource()).isFlagged() == false) {
+							((NewButton) e.getSource()).setGraphic(sixView);
+							((NewButton) e.getSource()).reveal();
+						}
+						if (count == 7 && !((NewButton) e.getSource()).isBomb() && isGameOn == true
+								&& ((NewButton) e.getSource()).isFlagged() == false) {
+							((NewButton) e.getSource()).setGraphic(sevenView);
+							((NewButton) e.getSource()).reveal();
+						}
+						if (count == 8 && !((NewButton) e.getSource()).isBomb() && isGameOn == true
+								&& ((NewButton) e.getSource()).isFlagged() == false) {
+							((NewButton) e.getSource()).setGraphic(eightView);
+							((NewButton) e.getSource()).reveal();
+						}
+						if (count == 0 && !((NewButton) e.getSource()).isBomb() && isGameOn == true
+								&& ((NewButton) e.getSource()).isFlagged() == false) {
+							((NewButton) e.getSource()).setGraphic(checkedTileView);
+							((NewButton) e.getSource()).reveal();
+						}
+						
+					}
+				});
 
 			}
 
@@ -99,9 +199,9 @@ public class minesweeper extends Application {
 			int dx = points[i];
 			int dy = points[++i];
 
-			int newX = button.getCol()+ dx;
-			int newY = button.getRow()+dy;
-			
+			int newX = button.getCol() + dx;
+			int newY = button.getRow() + dy;
+
 			if (newX >= 0 && newX <= 7 && newY >= 0 && newY <= 7) {
 				neighbours.add(board[newX][newY]);
 
@@ -109,21 +209,17 @@ public class minesweeper extends Application {
 		}
 		return neighbours;
 
-		
 	}
+
 	private int countNeighbours(ArrayList<NewButton> neighbours) {
 		int count = 0;
-		for(int i = 0; i < neighbours.size(); i++) {
+		for (int i = 0; i < neighbours.size(); i++) {
 			if (neighbours.get(i).isBomb()) {
-				count +=1;
-				
+				count += 1;
 			}
-			else {
-				
-			}
-			
+
 		}
 		return count;
 	}
-	
+
 }
